72f2eca9069044a26134934d993b8dc0d5edf23b
zhaojiaxing <zhaojiaxing@shixinyun.com>
2018-05-30 21:43:22 +0800

    <add>增加插入数据的sql

---
+++ insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO t_student VALUES(1,"张三",18,'男','2018-05-28','18125864478');
+
+INSERT INTO t_student(id,student_name,age,sex,birthday,tel) 
+VALUES(2,"李四",10,'男','2008-05-28','18215864478');
+
+INSERT INTO t_student(student_name,age,sex,birthday,tel) 
+VALUES("王五",11,'男','2007-05-28','18215864478');  
+
+INSERT INTO t_student(student_name,age,sex,birthday,tel) 
+VALUES("赵六",12,'男','2006-05-28','18215864478');
+
+INSERT INTO t_student(student_name,age,sex,birthday,tel) 
+VALUES("钱七",11,'男','2007-05-28','18215864478'),
+      ("李八",12,'男','2006-05-28','18215864478');
